87236cc438fd730c39ebd34a119bc319a9bccfd0
William Bushee <bluemonkeydev@gmail.com>
2018-01-07 20:47:55 -0600

    Update

---
+++ uno/SNx4HC138-3-Line-To-8-Line-Decoder/SNx4HC138-3-Line-To-8-Line-Decoder.ino
@@ -0,0 +1,91 @@
+const int selA = 2;
+const int selB = 3;
+const int selC = 4;
+const int enable = 5;
+
+void setup()
+{
+  // initialize the serial communication:
+  Serial.begin(9600);
+  
+  // initialize the control outputs
+  pinMode(selA, OUTPUT);
+  pinMode(selB, OUTPUT);
+  pinMode(selC, OUTPUT);
+  pinMode(enable, OUTPUT);
+  
+  digitalWrite(selA, LOW);
+  digitalWrite(selB, LOW);
+  digitalWrite(selC, LOW);
+  
+  digitalWrite(enable, HIGH);
+}
+
+void writeSignal(int i)
+{
+
+  if (i == 0) {
+    digitalWrite(enable, LOW);
+    digitalWrite(selC, LOW);
+    digitalWrite(selB, LOW);
+    digitalWrite(selA, LOW);
+    digitalWrite(enable, HIGH);
+
+  }else if (i == 1) {
+    digitalWrite(enable, LOW);
+    digitalWrite(selC, LOW);
+    digitalWrite(selB, LOW);
+    digitalWrite(selA, HIGH);
+    digitalWrite(enable, HIGH);
+    Serial.println("Green LED 1: HIGH");
+  
+  }else if (i == 2) {
+    digitalWrite(enable, LOW);
+    digitalWrite(selC, LOW);
+    digitalWrite(selB, HIGH);
+    digitalWrite(selA, LOW);
+    digitalWrite(enable, HIGH);
+    
+  }else if (i == 3) {
+    digitalWrite(enable, LOW);
+    digitalWrite(selC, LOW);
+    digitalWrite(selB, HIGH);
+    digitalWrite(selA, HIGH);
+    digitalWrite(enable, HIGH);
+    
+  }else if (i == 4) {
+    digitalWrite(enable, LOW);
+    digitalWrite(selC, HIGH);
+    digitalWrite(selB, LOW);
+    digitalWrite(selA, LOW);
+    digitalWrite(enable, HIGH);
+    
+  }else if (i == 5) {
+    digitalWrite(enable, LOW);
+    digitalWrite(selC, HIGH);
+    digitalWrite(selB, LOW);
+    digitalWrite(selA, HIGH);
+    digitalWrite(enable, HIGH);
+    
+  }else if (i == 6) {
+    digitalWrite(enable, LOW);
+    digitalWrite(selC, HIGH);
+    digitalWrite(selB, HIGH);
+    digitalWrite(selA, LOW);
+    digitalWrite(enable, HIGH);
+    
+  }else if (i == 7) {
+    digitalWrite(enable, LOW);
+    digitalWrite(selC, HIGH);
+    digitalWrite(selB, HIGH);
+    digitalWrite(selA, HIGH);
+    digitalWrite(enable, HIGH);
+    
+  }
+}
+
+void loop() {
+  writeSignal(random(0,8));
+  delay(100);
+}
+
